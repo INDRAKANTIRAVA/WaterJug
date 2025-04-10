8a71003da452d9c22771501107ce394edc703da0
INDRAKANTIRAVA <vindrakantirava_cse2205c5@gmail.com>
2025-04-10 19:07:38 +0530

    Initial Commit

---
+++ WaterJug/WaterJugApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterJugApp.swift
+//  WaterJug
+//
+//  Created by INDRA KANTI RAVA on 10/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterJugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
